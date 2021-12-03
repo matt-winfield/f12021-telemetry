a988e878edd45dacd60d0f3536111b3f89d6e942
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2021-12-03 16:41:44 +0000

    Use self hosted fonts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
 import PageContainer from './components/page-container';
 import useLiveDataConnection from './hooks/use-live-data-connection';
+import RobotoFont from './fonts/Roboto-Regular.ttf';
 
 const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'Roboto';
+        src: local('Roboto'), url(${RobotoFont}) format('truetype');
+    }
+
     html, body, #root {
         padding: 0;
         margin: 0;
